8acc56eb6c688a466bbd7699d7da5475e4bb8842
Alejandro Avilés <omegak@gmail.com>
2016-11-21 22:01:51 +0100

    Enter nunjucks

--- build.js
+++ build.js
@@ -4,6 +4,7 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var sass = require('node-sass');
+var nunjucks = require('nunjucks');
 var path = require('path');
 
 
@@ -13,32 +14,39 @@ var scssInPath = 'assets/style.scss';
 var scssOutPath = 'assets/static/style.css';
 
 sass.render({
-    file: scssInPath,
-    outFile: scssOutPath
-}, function(error, result) {
-    if(!error){
-        fs.writeFile(scssOutPath, result.css, function(err) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log(scssOutPath);
-        });
-    }
+  file: scssInPath,
+  outFile: scssOutPath
+}, function handleSassRender(err, res) {
+  if (err) { return console.log(err); }
+  fs.writeFile(scssOutPath, res.css, function(err) {
+    if (err) { return console.log(err); }
+    console.log(scssOutPath);
+  });
+});
+
+
+// -- Generate HTML ------------------------------------------------------------
+
+nunjucks.render('cv.njk', function handleNunjucksRender(err, res) {
+  if (err) { return console.log(err); }
+  var path = 'cv.html';
+  fs.writeFileSync(path, res);
+  console.log(path);
+  generatePdf(path);
 });
 
 
 // -- Generate PDF -------------------------------------------------------------
 
-var htmlPath = 'cv.html';
-var html = fs.readFileSync(htmlPath, 'utf8');
-var options = {
+function generatePdf(htmlPath) {
+  var html = fs.readFileSync(htmlPath, 'utf8');
+  var options = {
     format: 'Letter',
     base: 'file://' + path.resolve(htmlPath)
-};
+  };
 
-pdf.create(html, options).toFile('cv.pdf', function(err, res) {
-    if (err) {
-        return console.log(err);
-    }
+  pdf.create(html, options).toFile('cv.pdf', function handlePdfCreate(err, res) {
+    if (err) { return console.log(err); }
     console.log(res);
-});
+  });
+}
