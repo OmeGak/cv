568d05df9958953db0330f6b694f7e222fc1a7e2
Alejandro Avilés <omegak@gmail.com>
2016-11-23 02:20:34 +0100

    Code style

--- build.js
+++ build.js
@@ -1,52 +1,51 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var pdf = require('html-pdf');
-var sass = require('node-sass');
-var nunjucks = require('nunjucks');
-var path = require('path');
+var fs = require('fs')
+var pdf = require('html-pdf')
+var sass = require('node-sass')
+var nunjucks = require('nunjucks')
+var path = require('path')
 
 
 // -- Render SASS --------------------------------------------------------------
-
-var scssInPath = 'assets/style.scss';
-var scssOutPath = 'assets/static/style.css';
+var scssInPath = 'assets/style.scss'
+var scssOutPath = 'assets/static/style.css'
 
 sass.render({
   file: scssInPath,
   outFile: scssOutPath
 }, function handleSassRender(err, res) {
-  if (err) { return console.log(err); }
+  if (err) { return console.log(err) }
   fs.writeFile(scssOutPath, res.css, function(err) {
-    if (err) { return console.log(err); }
-    console.log(scssOutPath);
-  });
-});
+    if (err) { return console.log(err) }
+    console.log(scssOutPath)
+  })
+})
 
 
 // -- Generate HTML ------------------------------------------------------------
 
 nunjucks.render('cv.njk', function handleNunjucksRender(err, res) {
-  if (err) { return console.log(err); }
-  var path = 'cv.html';
-  fs.writeFileSync(path, res);
-  console.log(path);
-  generatePdf(path);
-});
+  if (err) { return console.log(err) }
+  var path = 'cv.html'
+  fs.writeFileSync(path, res)
+  console.log(path)
+  generatePdf(path)
+})
 
 
 // -- Generate PDF -------------------------------------------------------------
 
 function generatePdf(htmlPath) {
-  var html = fs.readFileSync(htmlPath, 'utf8');
+  var html = fs.readFileSync(htmlPath, 'utf8')
   var options = {
     format: 'Letter',
     base: 'file://' + path.resolve(htmlPath)
-  };
+  }
 
   pdf.create(html, options).toFile('cv.pdf', function handlePdfCreate(err, res) {
-    if (err) { return console.log(err); }
-    console.log(res);
-  });
+    if (err) { return console.log(err) }
+    console.log(res)
+  })
 }
